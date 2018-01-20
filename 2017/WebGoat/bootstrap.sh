7b0bb13a6189260e47a47951e567d086949eaf4e
a1617ns <a1617ns@aiit.ac.jp>
2018-01-20 15:31:43 +0900

    remove unnecessary modules

--- 2017/WebGoat/bootstrap.sh
+++ 2017/WebGoat/bootstrap.sh
@@ -3,18 +3,6 @@ apt-get update
 
 sudo apt-get install -y git wget curl
 
-echo "Installing Python 2.7.9"
-sudo apt-get install build-essential
-sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-mkdir -p ~/software
-cd ~/software
-wget https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz
-tar -xvf Python-2.7.9.tgz
-cd Python-2.7.9
-./configure
-make
-sudo make install
-
 echo "Installing docker and docker-compose"
 apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -23,13 +11,6 @@ apt-get update
 apt-get install -y docker-ce
 curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-
-echo "Installing node.js and npm"
-apt-get install -y linux-image-generic-lts-trusty
-apt-get install -y nodejs npm
-npm -g install n
-npm update npm -g
-n stable
 
 echo "Installing Suricata"
 add-apt-repository ppa:oisf/suricata-stable
